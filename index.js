d4952a4366a4344368cb98591d272829a3649c6e
Carriel-JT <78246105+Carriel-JT@users.noreply.github.com>
2021-05-28 16:19:38 -0300

    modulos

--- index.js
+++ index.js
@@ -1,44 +1,32 @@
-class cliente{
-    nome;
-    cpf;
-    rg;
-}
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
-class ContaCorrente{
-    agencia;
-    saldo;    
-
-    sacar(valor){
-        if(this.saldo >= valor){
-            this.saldo -= valor;
-        }
-    }
-}
-
-
-const cliente1 = new cliente();
+//clientes
+const cliente1 = new Cliente();
 cliente1.nome = "Jullian";
 cliente1.cpf = 11122233309;
 cliente1.rg = 123456789;
-console.log(cliente1);
-const constaCorrenteJullian = new ContaCorrente();
-constaCorrenteJullian.saldo = 0
-constaCorrenteJullian.agencia = 1001
-console.log(constaCorrenteJullian);
-constaCorrenteJullian.saldo += 100;
-console.log(constaCorrenteJullian);
-constaCorrenteJullian.sacar(50);
-console.log(constaCorrenteJullian);
-constaCorrenteJullian.sacar(200);
-console.log(constaCorrenteJullian);
 
-
-const cliente2 = new cliente();
+const cliente2 = new Cliente();
 cliente2.nome = "Luisa";
 cliente2.cpf = 99988877701;
 cliente2.rg = 987654321;
-console.log(cliente2);
-const constaCorrenteLuisa = new ContaCorrente();
-constaCorrenteLuisa.saldo = 0
-constaCorrenteLuisa.agencia = 1001
-console.log(constaCorrenteLuisa);
+
+//contas
+const conta1 = new ContaCorrente();
+conta1.agencia = 1001;
+conta1.cliente = cliente1;
+console.log(conta1);
+
+const conta2 = new ContaCorrente();
+conta2.agencia = 102;
+conta2.cliente = cliente2;
+console.log(conta2);
+
+//transações
+conta1.depositar(100);
+const valorSacado = conta1.sacar(50);
+conta1.depositar(500);
+
+let valor = 200;
+conta1.transferir(valor, conta2);
