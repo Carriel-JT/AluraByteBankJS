4dad211d6f06fb9b063e5e3b09c6cea839c11b49
Carriel-JT <78246105+Carriel-JT@users.noreply.github.com>
2021-05-27 21:50:31 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 class cliente{
     nome;
     cpf;
-    agencia;
-    saldo;
     rg;
 }
 
-const cliente1 = new cliente();
-const cliente2 = new cliente();
+class ContaCorrente{
+    agencia;
+    saldo;    
 
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+}
+
+
+const cliente1 = new cliente();
 cliente1.nome = "Jullian";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 cliente1.rg = 123456789;
+console.log(cliente1);
+const constaCorrenteJullian = new ContaCorrente();
+constaCorrenteJullian.saldo = 0
+constaCorrenteJullian.agencia = 1001
+console.log(constaCorrenteJullian);
+constaCorrenteJullian.saldo += 100;
+console.log(constaCorrenteJullian);
+constaCorrenteJullian.sacar(50);
+console.log(constaCorrenteJullian);
+constaCorrenteJullian.sacar(200);
+console.log(constaCorrenteJullian);
 
+
+const cliente2 = new cliente();
 cliente2.nome = "Luisa";
 cliente2.cpf = 99988877701;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 cliente2.rg = 987654321;
-
-console.log(cliente1);
 console.log(cliente2);
+const constaCorrenteLuisa = new ContaCorrente();
+constaCorrenteLuisa.saldo = 0
+constaCorrenteLuisa.agencia = 1001
+console.log(constaCorrenteLuisa);
