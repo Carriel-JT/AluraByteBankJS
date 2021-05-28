fbf65c67a828fe95164609baf8de108642914960
Carriel-JT <78246105+Carriel-JT@users.noreply.github.com>
2021-05-28 17:56:30 -0300

    Add files via upload

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,9 +1,26 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente{
     agencia;
-    cliente;
+    _cliente;
 
-    // #saldo = 0;
-    _saldo = 0;    
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+
+     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0;
+    
+    get saldo(){
+        return this._saldo;
+    }
 
     sacar(valor){
         if(this._saldo >= valor){
@@ -13,13 +30,17 @@ export class ContaCorrente{
     }
 
     depositar(valor){
-        if(valor <= 0) return;
-
-        this._saldo += valor;
+        if(valor <= 0)
+        {
+            return;
+        } 
+        this._saldo += valor;           
     }
 
-    transferir(valor,conta){
+    tranferir(valor, conta){
+        
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
+        
     }
 }
--- index.js
+++ index.js
@@ -1,32 +1,26 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-//clientes
 const cliente1 = new Cliente();
 cliente1.nome = "Jullian";
 cliente1.cpf = 11122233309;
-cliente1.rg = 123456789;
 
 const cliente2 = new Cliente();
-cliente2.nome = "Luisa";
-cliente2.cpf = 99988877701;
-cliente2.rg = 987654321;
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
 
-//contas
-const conta1 = new ContaCorrente();
-conta1.agencia = 1001;
-conta1.cliente = cliente1;
-console.log(conta1);
+
+const contaCorrenteJullian = new ContaCorrente();
+contaCorrenteJullian.agencia = 1001;
+contaCorrenteJullian.cliente = cliente1;
+contaCorrenteJullian.depositar(500);
 
 const conta2 = new ContaCorrente();
+conta2.cliente = cliente1;
 conta2.agencia = 102;
-conta2.cliente = cliente2;
-console.log(conta2);
-
-//transações
-conta1.depositar(100);
-const valorSacado = conta1.sacar(50);
-conta1.depositar(500);
 
 let valor = 200;
-conta1.transferir(valor, conta2);
+contaCorrenteJullian.tranferir(valor, conta2);
+
+conta2.saldo = 30000;
+console.log(conta2.saldo);
